e2e2806422d7c9c9cba0979b6a672d24c4c82d16
Pete Hurley <p.d.hurley@sussex.ac.uk>
2021-04-26 16:15:21 +0100

    empty line for tables to render

--- docs/notebooks/modelling/make_website.py
+++ docs/notebooks/modelling/make_website.py
@@ -6,10 +6,10 @@ mdFile.new_paragraph("The fits for the different fields can be found at the foll
 
 field=['Lockman-SWIRE','ELAIS-S1','XMM-LSS']
 for i in field:
-    print(i)
     mdFile.new_header(level=1, title=i)
-    markdown=MDTable('../../../data/MRR2018_tables/{}_sources.csv'.format(i))
+    mdFile.write('  \n')
+    markdown=MDTable('../../../data/MRR2018_tables/{}_web.csv'.format(i))
     markdown_string_table = markdown.get_table()
     mdFile.write(markdown_string_table)
-    mdFile.new_paragraph()
+    mdFile.write('  \n')
 mdFile.create_md_file()
