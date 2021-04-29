044e32604fc4f857b83f6c47d940d52dff120856
Pete Hurley <p.d.hurley@sussex.ac.uk>
2021-04-29 08:49:00 +0100

    add fitted, converted notebooks

--- docs/notebooks/modelling/make_website.py
+++ docs/notebooks/modelling/make_website.py
@@ -1,11 +1,15 @@
 from mdtable import MDTable
 from mdutils.mdutils import MdUtils
-
+import os
+import glob
 mdFile = MdUtils(file_name='fits',title='Fits')
 mdFile.new_paragraph("The fits for the different fields can be found at the following locations:")
 
 field=['Lockman-SWIRE','ELAIS-S1','XMM-LSS']
 for i in field:
+    srcs=glob.glob('./output/{}/*.ipynb'.format(i))
+    for s in range(1,len(srcs)+1):
+        os.system('jupyter nbconvert ./output/{}/fit_{}.ipynb --to markdown'.format(i,s))
     mdFile.new_header(level=1, title=i)
     mdFile.write('  \n')
     markdown=MDTable('../../../data/MRR2018_tables/{}_web.csv'.format(i))
